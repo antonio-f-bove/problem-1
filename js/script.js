a55570ac18c42261ff72f4c3bdb2b1eefbfd3e24
Antonio Bove <antoniofbove@gmail.com>
2022-07-27 22:23:02 +0200

    bonus - needs fixing in #first-col

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
-const NUMBER_OF_CELLS = 25
+const NUMBER_OF_CELLS_IN_THIRD_COL = 25
+const NUMBER_OF_CELLS_IN_MINI_GRID = 4
 
 // TODO non ho capito perché ho avuto bisogno di specificare di aspettare l'onload
 window.addEventListener('load', () => {
-  fillGrid(NUMBER_OF_CELLS)
-  assignCellsRandomColour()
+  const grid = document.querySelector('#third-col .grid')
+  fillGrid(grid, NUMBER_OF_CELLS_IN_THIRD_COL)
+
+  const cells = document.querySelectorAll('.cell')
+  cells.forEach(cell => {
+    assignCellRandomColour(cell)
+
+    cell.addEventListener('click', (e) => {
+      const miniGrid = document.createElement('div')
+      miniGrid.className = 'grid-4x4'
+      fillGrid(miniGrid, NUMBER_OF_CELLS_IN_MINI_GRID)
+
+      const newCells = miniGrid.children      
+      for (let i = 0; i < newCells.length; i++) {
+        assignCellRandomColour(newCells[i])
+      }
+      
+      e.target.append(miniGrid)
+      console.log(e.target)
+    })
+  })
 })
 
 
-function fillGrid(numberOfCells) {
-  const grid = document.querySelector('#third-col .grid')
-
+function fillGrid(grid, numberOfCells) {
   for (let i = 0; i < numberOfCells; i++) {
     const cell = makeNewCell()
     grid.appendChild(cell)
@@ -23,11 +41,9 @@ function makeNewCell() {
   return cell
 }
 
-function assignCellsRandomColour() {
-  const cells = document.querySelectorAll('.cell')
-  cells.forEach(cell => {
-    cell.style.backgroundColor = getRandomHexColour()
-  })
+function assignCellRandomColour(cell) {
+  cell.style.backgroundColor = getRandomHexColour()
+
 }
 
 function getRandomHexColour() {
