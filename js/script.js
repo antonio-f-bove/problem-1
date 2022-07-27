f1049e9abcad8cab38d6af67bb89aeca8ee8ca53
Antonio Bove <antoniofbove@gmail.com>
2022-07-27 21:54:03 +0200

    layout refactoring + media queries + random colours

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@ const NUMBER_OF_CELLS = 25
 
 // TODO non ho capito perché ho avuto bisogno di specificare di aspettare l'onload
 window.addEventListener('load', () => {
-  const grid = document.querySelector('.grid')
-  fillGrid(grid, NUMBER_OF_CELLS)
+  fillGrid(NUMBER_OF_CELLS)
+  assignCellsRandomColour()
 })
 
 
-function fillGrid(grid, numberOfCells) {
+function fillGrid(numberOfCells) {
+  const grid = document.querySelector('#third-col .grid')
+
   for (let i = 0; i < numberOfCells; i++) {
     const cell = makeNewCell()
     grid.appendChild(cell)
@@ -19,4 +21,25 @@ function makeNewCell() {
   cell.className = 'cell'
 
   return cell
+}
+
+function assignCellsRandomColour() {
+  const cells = document.querySelectorAll('.cell')
+  cells.forEach(cell => {
+    cell.style.backgroundColor = getRandomHexColour()
+  })
+}
+
+function getRandomHexColour() {
+  let hexColour = '#'
+  for (let i = 0; i < 6; i++) {
+    hexColour += getRandomHexCharacter()
+  }
+
+  return hexColour
+}
+
+function getRandomHexCharacter() {
+  const charPool = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+  return charPool[Math.floor(Math.random() * 16)]
 }
